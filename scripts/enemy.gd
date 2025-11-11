7c0f42d3edffa18a450a1a971dea734c4429b415
Scott Sheffield <scottux@gmail.com>
2025-11-11 14:33:38 -0500

    fish chases you now

---
+++ scripts/enemy.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed = 180
+var playerNode
+
+func _ready():
+	playerNode = get_node("../Player")
+	
+func _process(delta):
+	velocity = (playerNode.position - position) / position.distance_to(playerNode.position) * speed
+	move_and_slide()
