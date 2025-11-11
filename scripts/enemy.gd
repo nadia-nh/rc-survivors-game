85b6a60b3ffe5e1c254ea438007c7ca0993d61c4
Scott Sheffield <scottux@gmail.com>
2025-11-11 15:37:26 -0500

    Fish now attack en masse

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 180
+@export var speed = 150
 var playerNode
 
+func _init():
+	print('initialized')
+
 func _ready():
-	playerNode = get_node("../Player")
+	print('enemy spawned at', position)
+	playerNode = get_node("../../Player")
 	
 func _process(delta):
 	velocity = (playerNode.position - position) / position.distance_to(playerNode.position) * speed
